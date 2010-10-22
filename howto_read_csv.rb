d3affdab0f42eef8a56863a91d9da52bdad786c6
Craig H Anderson <crai@coot.net>
2010-10-22 05:46:23 -0600

    student_availability.rb will read student_availability.csv

---
+++ howto_read_csv.rb
@@ -0,0 +1,12 @@
+require 'csv'
+data_file = 'student_availability.csv'
+row_count = 0
+CSV.foreach(data_file) do |row|
+  row_count += 1
+ puts "row.class=#{row.class} row.length=#{row.length}  === #{row.to_s}"
+end
+puts "#{row_count} rows"
+=begin
+student_availability.csv - 19 rows, 1 row with column names, 18 student rows
+Row 1, Column names: ["Name", "Monday Availability", "Wednesday Availability"]
+=end
---
+++ student_availability.rb
@@ -0,0 +1,23 @@
+require 'csv'
+
+module RmuEntranceExam
+  TEST_FILE_NAME = ''
+  class StudentAvailability
+    def initialize
+      @name = ''
+      @monday_times = []
+      @wednesday_times = []
+    end
+    def StudentAvailability.from_csv_row(csv_row)
+      student_availability = StudentAvailability.new
+      student_availability.name = row[0]
+    end
+    def StudentAvailability.load_csv_file(csv_file=RmuEntranceExam::TEST_FILE_NAME)
+      loaded_file = CSV.read(csv_file)
+      loaded_file[1..-1].map {|row| StudentAvailability.from_csv_row(row)}
+    end
+    def StudentAvailability.times_from_availability_column(availability_column)
+      'Times not parsed yet'
+    end
+ end
+end
